c5c9b0beecdbe94196be2b0dfd4e4292e142dd67
PoojaRawt <poojarwt852@gmail.com>
2024-11-05 16:00:37 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
+// import './App.css'
+// import Home from './pages/home/Home.jsx';
+// // import Login from './pages/login/<Login>.jsx';
+// // import SignUp from './pages/signup/SignUp.jsx';
+
+// function App() {
+ 
+//   return (
+//     <div className='p-4 h-screen flex items-center justify-center'>
+//       {/* <Routes>
+//         <Routes path='/'element={<Home/>}/>
+//         <Routes path='/login'element={<Login/>}/>
+//         <Routes path='/signup'element={<SignUp/>}/>
+//       </Routes> */}
+//       <Home/>
+//     </div>
+//   )
+// }
+
+// export default App
 import './App.css'
 import Home from './pages/home/Home.jsx';
-import Login from './pages/login/<Login>.jsx';
-import SignUp from './pages/signup/SignUp.jsx';
 
 function App() {
  
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Routes>
-        <Routes path='/'element={<Home/>}/>
-        <Routes path='/login'element={<Login/>}/>
-        <Routes path='/signup'element={<SignUp/>}/>
-
-      </Routes>
+      <Home/>
     </div>
   )
 }
 
-export default App
+export default App
