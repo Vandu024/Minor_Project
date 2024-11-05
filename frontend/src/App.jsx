5ada4b915b75158909278718c3c60c4610a2ba2a
PoojaRawt <poojarwt852@gmail.com>
2024-11-05 14:36:16 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Home from './pages/home/Home.jsx';
+import Login from './pages/login/<Login>.jsx';
+import SignUp from './pages/signup/SignUp.jsx';
 
 function App() {
  
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home/>
+      <Routes>
+        <Routes path='/'element={<Home/>}/>
+        <Routes path='/login'element={<Login/>}/>
+        <Routes path='/signup'element={<SignUp/>}/>
+
+      </Routes>
     </div>
   )
 }
