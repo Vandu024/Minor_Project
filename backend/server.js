c5925558a61b1470d2e922e24f8bb4f9691fe581
Vandana Uniyal <vandanauniyal123456@gmail.com>
2024-12-10 18:29:19 +0530

    development commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,24 +11,25 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import {app,server} from "./socket/socket.js";
 
 
+dotenv.config();
+const __dirname = path.resolve();
+
+
 const PORT  = process.env.PORT || 5000;
 
-dotenv.config();
 
 app.use(express.json());  // to parse the incoming requests with json payloads (from req.body)
 app.use(cookieParser()); // to parse the incoming cookies from req.bodies
-
-
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-
-// app.get("/" , (req,res)=>{
-//     res.send("Hello World!");
-// });
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 
 server.listen(PORT, ()=>{
